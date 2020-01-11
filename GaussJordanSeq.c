4e7ba727d7e5cf1d590ddb2331e361aa9ea3ebb1
aurelien <a.delval87@gmail.com>
2020-01-12 00:20:06 +0100

    Improve & correct code

--- GaussJordanSeq.c
+++ GaussJordanSeq.c
@@ -1,177 +1,202 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-float* Normalization(float * vect1, float coef,int dim);
-float* Simplification(float * vect1, float * vect2, float valeur, int dim);
-float* Diagonalization(float * vect1,float valeur,float * vect2,int dim);
-float Diagonalization2(float vect1,float valeur,float * vect2,int dim);
-void FillMatrix(float *** matrix, int dim);
-void FillVector(float ** vect, int dim);
-void DisplayMatrix(float ** matrix, int dim);
-void DisplayVector(float * vector, int dim);
-void DivVector(float *** matrix ,float coef, int dim,float * vect);
-void FreeTab(float ** matrix,float * vect,int dim);
 
-int main(){
- int dim = 0;
- float max = 0;
- float ** matrixA;
- float ** matrixAcopy;
- float * temp;
- float temp2;
- float * vectB;
- //Size of matrix :
- printf("Ordre n de la matrice carrée A ? \n");
- scanf(" %d",&dim);
- matrixA = (float**)calloc(dim,sizeof(float*));
- vectB = (float*)calloc(dim,sizeof(float));
- FillMatrix(&matrixA,dim);
- FillVector(&vectB,dim);
- DisplayMatrix(matrixA,dim);
- DisplayVector(vectB,dim);
- //Choice of pivot at line K :
- int r = -1, j = 0, i = 0, k = 0;
- for (int j = 0 ; j < dim; j++){
-   max = matrixA[r+1][j];
-   k=r+1;
-   // looking for max :
-   for (int i =r+1; i < dim; i++){
-     if (matrixA[i][j] > max){
-       max = matrixA[i][j];
-       k = i;
-     }
-  //   printf("max = %f , k = %d \n",max,k );
-   }
-   // if pivot not null :
-   if (matrixA[k][j] != 0){
-        r++;
-     // Normalization of the line to get echelon form :
-      matrixA[k] = Normalization(matrixA[k],matrixA[k][j],dim);
-      // printf("après Normalization: r = %d, k = %d \n",r,k );
-      // DisplayMatrix(matrixA,dim);
-    //  DisplayVector(vectB,dim);
-     if(k!=r){
-       temp = matrixA[k];
-       matrixA[k] = matrixA[r];
-       matrixA[r] = temp;
-       temp2 = vectB[k];
-       vectB[k] = vectB[r];
-       vectB[r] = temp2;
-     }
-     /*
-       printf("Avant Simplification :\n");
-       DisplayMatrix(matrixA,dim);
-       DisplayVector(vectB,dim);*/
 
-       // Simplification of other lines :
-       for (int i = 0 ; i < dim; i++){
-         if(i!=r){
-              vectB[i] = vectB[i]-(vectB[r]*matrixA[i][j]);
-              matrixA[i] = Simplification(matrixA[i],matrixA[r],matrixA[i][j],dim);
-         }
-       }
-    /*   printf("Après Simplification :\n");
-       DisplayMatrix(matrixA,dim);
-       DisplayVector(vectB,dim);*/
+void Normalization(float ** Ak,float Akj, int n);
+void Simplification(float ** vect1, float ** vect2, float valeur, int n);
+void DiagonalizationMat(float ** Aj, float * Aji, float ** Ai, int n);
+void DivVector(float *** matrix ,float coef, int n,float * vect);
 
-   }
-   printf("Avant Diagonalization :\n");
-   DisplayMatrix(matrixA,dim);
-   DisplayVector(vectB,dim);
-   //Diagonalization : #TODO : corriger la diagonalisation ? vérifier les valeurs de b.
-   for(int i = dim-1; i>=1 ; i--) {
-     for (int j = i-1 ; j>=0;j--) {
-        matrixA[j] = Diagonalization(matrixA[j],matrixA[j][i],matrixA[i],dim);
-        vectB[j] = Diagonalization2(vectB[j],vectB[i],matrixA[i],dim);
-     }
-   }
-   printf("Après Diagonalization :\n");
-   DisplayMatrix(matrixA,dim);
-   DisplayVector(vectB,dim);
+void FillRandomMatrix(float *** matrix, int n);
+void FillVector(float ** vect, int n);
+void FillRandomVector(float ** vect, int n);
+void FreeTab(float ** matrix,float * vect,int n);
 
- }
- printf("\n Fin : \n");
- DisplayMatrix(matrixA,dim);
- printf("\n");
- DisplayVector(vectB,dim);
- FreeTab(matrixA,vectB,dim);
- return 0;
-}
+void DisplayMatrix(float ** matrix, int n);
+void DisplayVector(float * vector, int n);
 
-float* Diagonalization(float * vect1,float valeur,float * vect2,int dim){
-  for (int i = 0 ; i <dim ; i++){
-    vect1[i] = vect1[i]-(valeur*vect2[i]);
+
+
+int main(void) {
+    int n = 0;
+    float max = 0;
+    float ** A;
+    float * temp;
+    float temp2;
+    float * b;
+
+
+		//Step 0: Matrix initialization
+    printf("Ordre n de la matrice carrée A ? \n");
+    scanf(" %d",&n);
+
+	A = (float**)calloc(n,sizeof(float*));
+    b = (float*)calloc(n,sizeof(float));
+    FillRandomMatrix(&A,n);
+    FillRandomVector(&b,n);
+
+    A[0][0] = 1;
+    A[0][1] = 2;
+    A[1][0] = 3;
+    A[1][1] = 4;
+
+    b[0] = 5;
+    b[1] = 6;
+
+    printf("Initial system:\n");
+	DisplayMatrix(A, n);
+    DisplayVector(b, n);
+
+
+		//Step 1: Solving system
+    int r = -1, k = 0;
+    for (int j=0; j<n; j++) {
+
+		max = A[r+1][j];
+        k=r+1;
+
+		//Looking for max
+        for (int i=r+1; i<n; i++) {
+            if (A[i][j] > max) {
+                max = A[i][j];
+                k = i;
+            }
+        }
+
+		//If pivot not null
+        if (A[k][j] != 0) {
+            r++;
+
+            // Normalization of the line to get reduced row echelon form
+            b[k] = b[k] / A[k][j];
+
+            Normalization( &(A[k]), A[k][j], n );
+
+            if(k!=r) {
+                temp = A[k];
+                A[k] = A[r];
+                A[r] = temp;
+                temp2 = b[k];
+                b[k] = b[r];
+                b[r] = temp2;
+            }
+
+            // Simplification of other lines
+            for (int i = 0 ; i < n; i++) {
+                if(i != r) {
+                    b[i] = b[i] - b[r] * A[i][j];
+
+                    Simplification(&(A[i]), &(A[r]), A[i][j], n);
+                }
+            }
+        }
     }
-  return vect1;
+
+    printf("Solution:\n");
+    DisplayMatrix(A, n);
+    DisplayVector(b, n);
+
+    FreeTab(A, b, n);
+
+	return 0;
 }
-float Diagonalization2(float vect1,float valeur,float * vect2,int dim){
-  for (int i = 0 ; i <dim ; i++){
-    vect1=vect1-(valeur*vect2[i]);
+
+
+
+void DiagonalizationMat(float ** Aj, float * Aji, float ** Ai, int n) {
+    for (int k = 0 ; k<n ; k++) {
+        *(Aj[k]) = *(Aj[k]) - *(Ai[k]) * (*Aji);
     }
-  return vect1;
 }
 
-float* Simplification(float * vect1, float * vect2, float valeur, int dim){
-  for (int i = 0 ; i <dim ; i++){
-    vect1[i] = vect1[i]-(vect2[i]*valeur);
+
+
+void Simplification(float ** Ai, float ** Ar, float Aij, int n) {
+    for (int i = 0; i<n; i++) {
+        (*Ai)[i] = (*Ai)[i] - (*Ar)[i] * Aij;
     }
-  return vect1;
 }
 
-float * Normalization(float * vect1,float coef, int dim){
-  for (int i = 0 ; i <dim ; i++){
-    vect1[i] = vect1[i]/coef;
+
+
+void Normalization(float ** Ak, float Akj, int n) {
+    for (int i = 0; i < n; i++) {
+        (*Ak)[i] = (*Ak)[i] / Akj;
     }
-  return vect1;
-
 }
 
-void FillMatrix(float *** matrix, int dim){
-  float val;
-  srand (time (NULL));
-  for (int i=0 ; i < dim ; i++) {
-    (*matrix)[i] = (float*)calloc(dim,sizeof(float));
-  }
-  for (int i = 0 ; i < dim ; i++){
-    for(int j = 0 ; j < dim ; j++){
-      //printf("valeur de la matrice A [%d][%d] ? : \n",i,j);
-    //  scanf(" %f",&val);
+
+
+void FillRandomMatrix(float *** matrix, int n) {
+    float val;
+    srand (time (NULL));
+
+    for (int i=0 ; i < n ; i++) {
+        (*matrix)[i] = (float*)calloc(n,sizeof(float));
+    }
+
+    for (int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < n ; j++) {
+            val = rand()%100;
+            (*matrix)[i][j] = val;
+        }
+    }
+}
+
+
+
+void FillVector(float ** vect, int n) {
+    float val;
+    for (int i = 0 ; i < n ; i++) {
+        printf("Value of b [%d] ? : \n",i);
+        scanf(" %f",&val);
+        (*vect)[i] = val;
+    }
+}
+
+
+
+void FillRandomVector(float ** vect, int n) {
+    float val;
+
+    (*vect) = (float*)calloc(n,sizeof(float));
+
+    for(int i = 0 ; i < n ; i++) {
         val = rand()%100;
-      (*matrix)[i][j] = val;
-    }
-  }
-}
-
-void FillVector(float ** vect, int dim){
-  float val;
-  for (int i = 0 ; i < dim ; i++){
-      printf("valeur du vecteur b [%d] ? : \n",i);
-      scanf(" %f",&val);
-      (*vect)[i] = val;
+        (*vect)[i] = val;
     }
 }
 
-void DisplayMatrix(float ** matrix, int dim){
-  for (int i = 0 ; i < dim ; i++){
-    for (int j = 0 ; j < dim ; j++){
-      printf(" %f \t",matrix[i][j]);
+
+
+void DisplayMatrix(float ** matrix, int n) {
+    printf("Matrix A :\n");
+    for (int i = 0 ; i < n ; i++) {
+        for (int j = 0 ; j < n ; j++) {
+            printf(" %f \t",matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+void DisplayVector(float * vector, int n) {
+    printf("Vector b :\n");
+    for (int i = 0 ; i < n ; i++) {
+        printf(" %f \t",vector[i]);
+        printf("\n");
     }
     printf("\n");
-  }
 }
 
-void DisplayVector(float * vector, int dim){
-  printf("\nValeur du vecteur b :\n");
-  for (int i = 0 ; i < dim ; i++){
-      printf(" %f \t",vector[i]);
-    printf("\n");
-  }
-}
 
-void FreeTab(float ** matrix,float * vect,int dim){
-  for (int i = 0 ; i < dim ; i++){
-    free(matrix[i]);
-  }
-  free(matrix);
-  free(vect);
+
+void FreeTab(float ** matrix,float * vect,int n) {
+    for (int i = 0 ; i < n ; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+    free(vect);
 }
